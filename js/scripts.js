f82cb9b289f37b74578091d6b675b653f2043c4e
Kayla Ondracek and Michael Albers <kayla@>
2018-01-16 09:07:59 -0800

    add form where users can submit favorite items, display favorite items, and see them in a different order

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-  
+  $("#favorite").submit(function(event) {
+    var favorites = [$("#food").val(), $("#animal").val(), $("#vacation").val(), $("#color").val()];
+
+    var otherFavorites = [];
+    otherFavorites.push(favorites[1], favorites[0], favorites[2]);
+    console.log(otherFavorites);
+
+    $("#user-favorites").html("<p>This is an array of all your favorite things</p>" + favorites.join(", ") + "<p>Here are the second, first, and third elements in your array</p><ul><li>" + otherFavorites[0] + "</li><li>" + otherFavorites[1] + "</li><li>" + otherFavorites[2] + "</li></ul>");
+
+    //console.log(favorites);
+    event.preventDefault();
+  });
 });
